4f7f9ecade85d5488dcf54c3a29d446120b6bdf3
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-02-22 23:51:21 -0600

    fix map api

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { Paper, Typography, useMediaQuery} from '@material-ui/core';
+import { Paper, requirePropFactory, Typography, useMediaQuery} from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-
 import useStyles from './styles';
 
 const Map = () => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width: 600px');
   const coordinates = {lat: 30.527700, lng: -97.670970};
+  const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyDBs-tlaOvJUsBZ6JqFd3IieijQ-Pnga5g'}}
+        bootstrapURLKeys={{ key: API_KEY}}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={15}
